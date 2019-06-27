7cb3ca9f752c737229bb4d56e625944e49bc5e2d
Jonas Pickholtz <jpickhol@gmail.com>
2019-06-27 17:02:46 +0300

    added Ract skeleton and function that adds input to toDo list

--- js/to_do.js
+++ js/to_do.js
@@ -1,4 +1,4 @@
-class App extends React.Component {
+class Done extends React.Component {
     constructor(props) {
         super(props);
 
@@ -7,7 +7,96 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                Hello World
+                Done list
+            </div>
+        );
+    }
+}
+
+class ToDo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userInput: "",
+            itemArray: []
+        }
+        this.handleUserInput = this.handleUserInput.bind(this);
+        this.addItemToList = this.addItemToList.bind(this);
+    }
+    handleUserInput(e) {
+        this.setState({
+            userInput: e.target.value
+        })
+    }
+
+    addItemToList() {
+        this.state.itemArray.push(this.state.userInput);
+        this.setState({
+            userInput: ""
+        })
+    }
+    render() {
+
+        return (
+            <div>
+                <input
+                    id="inputBox"
+                    placeholder="add item to list..."
+                    value={this.state.userInput}
+                    onChange={this.handleUserInput}
+                />
+                <button onClick={this.addItemToList}> Add</button>
+                <ul>
+                    {this.state.itemArray.map((activity, index) => <li key={index}>{activity}</li>)}
+                </ul>
+            </div>
+        );
+    }
+}
+
+class Body extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <ToDo />
+                <Done />
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                Header-
+                To Do: Make Header with Logo
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <Body />
             </div>
         );
     }
